53c87ad3626f6862780d2e6363ea8bb8dfd81c8f
Nikita <ilirion92@gmail.com>
2025-04-24 22:34:02 +0200

    upd: optimization, added redis, reducing LCP.

---
+++ src/database/migrations/2025_04_23_184949_add_columns_to_coins_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('coins', function (Blueprint $table) {
+            $table->bigInteger('volume_24h')->nullable()->after('market_cap_rank');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('coins', function (Blueprint $table) {
+            $table->dropColumn('volume_24h');
+        });
+    }
+};
