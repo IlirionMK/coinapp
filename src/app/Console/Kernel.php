4ee1347f9625327d6a785c84892da842630e7c1a
Nikita <ilirion92@gmail.com>
2025-04-27 19:59:55 +0200

    added icons, modified coins.

--- src/app/Console/Kernel.php
+++ src/app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\IconsSyncCommand;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use App\Console\Commands\SyncCoinsCommand;
