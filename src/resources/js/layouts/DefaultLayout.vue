a8e9d4138ba9023ce9f8b56f69ed40d3b538b1a4
Nikita <ilirion92@gmail.com>
2025-05-07 23:53:42 +0200

    auth fixed

--- src/resources/js/layouts/DefaultLayout.vue
+++ src/resources/js/layouts/DefaultLayout.vue
@@ -1,20 +1,16 @@
 <template>
     <div class="flex flex-col min-h-screen bg-gray-50 text-gray-900">
-        <!-- Header -->
-        <Header />
+         <Header />
 
-        <!-- Navigation -->
-        <Navigation />
+         <Navigation />
 
-        <!-- Main Content Container -->
-        <main class="flex-1">
+         <main class="flex-1">
             <div class="max-w-screen-xl mx-auto w-full px-4 py-6">
                 <RouterView />
             </div>
         </main>
 
-        <!-- Footer -->
-        <Footer />
+         <Footer />
     </div>
 </template>
 
--- src/routes/api.php
+++ src/routes/api.php
@@ -1,7 +1,13 @@
-use App\Http\Controllers\AuthController;
+<?php
 
-Route::middleware('guest')->post('/login', [AuthController::class, 'login']);
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Api\CoinController;
+
+ Route::middleware('guest')->post('/login', [AuthController::class, 'login']);
 Route::middleware('guest')->post('/register', [AuthController::class, 'register']);
 
-Route::middleware('auth:sanctum')->get('/user', [AuthController::class, 'user']);
+ Route::middleware('auth:sanctum')->get('/user', [AuthController::class, 'user']);
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
+
+ Route::get('/coins', [CoinController::class, 'index']);
